a55648ed08039c466cb6297770ed6d48422a5d85
Filip Bajraktari <fbajraktari@tenstorrent.com>
2026-06-22 12:16:55 +0000

    Turned off 2-link tests in tt_moe

--- models/demos/deepseek_v3_d_p/tests/pcc/test_ttnn_moe.py
+++ models/demos/deepseek_v3_d_p/tests/pcc/test_ttnn_moe.py
@@ -685,6 +685,7 @@ def test_ds_moe(
     gate_fallback_mode,
     request,
 ):
+    num_links = 1  # Only temporarily here for debugging purpose (See #47650)
     run_model(
         variant,
         config_only,
@@ -780,6 +781,7 @@ def test_kimi_moe(
     gate_fallback_mode,
     request,
 ):
+    num_links = 1  # Only temporarily here for debugging purpose (See #47650)
     run_model(
         variant,
         config_only,
